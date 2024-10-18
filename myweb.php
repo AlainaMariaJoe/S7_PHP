4bd093352d2c2f7f7d5774f6656cab81dababac3
AlainaMariaJoe <alainamariajoe33@gmail.com>
2024-10-18 09:55:44 +0530

    Add files via upload

---
+++ myweb.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+
+    <nav>
+        <a href="myweb.php">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="studentlogin.php">LOGIN</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="addstudent.php">ADD</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="updatestudent.php">UPDATE</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="deletestudent.php">DELETE</a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </nav>
+
+    <?php
+    echo "<h1>Welcome to the Home Page</h1>";
+    ?>
+    <img src="Sjcet.jpg" alt="Sjcet" width="600" height="400">>
+
+</body>
+</html>
