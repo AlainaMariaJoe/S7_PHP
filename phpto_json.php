09e5e1ea083cc06293c292de16f96fd2cfe4801d
AlainaMariaJoe <alainamariajoe33@gmail.com>
2024-11-24 23:02:08 +0530

    encode PHP data to JSON

--- phpto_json.php
+++ phpto_json.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP to JSON Example</title>
+</head>
+<body>
+    <h1>PHP to JSON Output</h1>
+    <?php
+      $array = array("name" => "John", "age" => 30);
+      $json = json_encode($array);
+      echo $json; 
+    ?>
+</body>
+</html>
  
